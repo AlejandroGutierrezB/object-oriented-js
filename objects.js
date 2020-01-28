bff8d6cccb4d9065e97b873cf20dc97a26bb183f
Alejandro Gutierrez <alejandrogutbarce@gmail.com>
2020-01-28 18:05:54 +0100

    Lesson 11

--- objects.js
+++ objects.js
@@ -13,7 +13,22 @@ User.prototype.logout = function () {
     console.log(this.email, 'has logged out');
 };
 
+function Admin(...args) { // ...args make the input from "new Admin" into an array, so later args is used instead of hardtyping all the argumetns in the array
+    User.apply(this, args); //"this" binds the context of the "new Admin" to apply it into the "function User", so later it inherits the other "this." 
+    this.role = 'super admin';
+}
+
+Admin.prototype = Object.create(User.prototype);
+Admin.prototype.deleteUser = function (u) {
+    users = users.filter(users => {
+        return user.email != u.email;
+    });
+};
+
 var userOne = new User('ryan@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
-console.log(userOne);
-userTwo.login();
+var admin = new Admin('shaun@ninjas.com', 'Shaun');
+
+var users = [userOne, userTwo, admin];
+console.log(admin);
+
