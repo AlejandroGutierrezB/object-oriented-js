c4a0b6f1357003175b94b8282a19bcea755263ed
Alejandro Gutierrez <alejandrogutbarce@gmail.com>
2020-01-28 16:49:19 +0100

    Lesson 8

--- objects.js
+++ objects.js
@@ -19,8 +19,19 @@ class User {
     }
 }
 
+class Admin extends User {
+    deleteUser(user) {
+        users = users.filter(u => {
+            return u.email != user.email; // if not equal it will return true so the filter wont do anything
+        });
+
+    }
+}
 var userOne = new User('ryan@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+var admin = new Admin('alex@ninjas.com', 'Alex');
+var users = [userOne, userTwo, admin];
 
-userOne.login().updateScore().updateScore().logout();
-// userTwo.login().logout();
+admin.deleteUser(userTwo);
+
+console.log(users);
