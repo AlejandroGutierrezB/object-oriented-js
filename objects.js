f27c2a890c0ddb584d1b2a9bdcd456511fd1e76e
Alejandro Gutierrez <alejandrogutbarce@gmail.com>
2020-01-28 16:20:04 +0100

    Lesson 5

--- objects.js
+++ objects.js
@@ -1,3 +1,10 @@
 class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
 
+    }
 }
+
+var userOne = new User('ryan@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
